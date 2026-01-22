cbd6b0ce4f1535cb5e3eca56a7aa3dc983454f24
Kyle Reid <kyle.reid@gmail.com>
2026-01-22 17:26:02 -0500

    Add nvme util.

--- configuration.nix
+++ configuration.nix
@@ -79,6 +79,7 @@
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      parted
+     nvme-cli
      git # Needed for Flakes.
   ];
 
