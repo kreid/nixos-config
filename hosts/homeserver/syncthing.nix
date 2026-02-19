00405ef7bc85afc992a05ae7d9a4ac603e116700
Kyle Reid <kyle.reid@gmail.com>
2026-02-19 00:51:37 -0500

    feat(syncthing): add windows desktop

--- hosts/homeserver/syncthing.nix
+++ hosts/homeserver/syncthing.nix
@@ -13,15 +13,16 @@
     configDir = "/home/kreid/.config/syncthing";
     settings = {
       devices = {
-        "Home server" = { id = "YSSVO6U-OSLARLV-OQCRIW6-GALXLM6-D5YTS4A-YUBF6HI-ZOFQMFC-O2C6NQL"; };
-        "MacBook Air" = { id = "FBWYNLJ-VALD4DG-KTPZ4HG-AUCST5P-NOPJETV-F4I4WLM-MGRMXRS-XAHGLA2"; };
-        "MacBook Pro" = { id = "2NM42ZS-OPYWZNW-VA42QTK-2RKIB5Y-3QNBNPT-KNH6LJL-L7Q2BPI-PK6YKAZ"; };
+        "Home server"     = { id = "YSSVO6U-OSLARLV-OQCRIW6-GALXLM6-D5YTS4A-YUBF6HI-ZOFQMFC-O2C6NQL"; };
+        "Windows desktop" = { id = "ULLQSIT-2R3FZVL-P4F2IQX-PW5E5AQ-J6DGPG6-4LN6VFG-2CIXINP-DG3VXQD"; };
+        "MacBook Air"     = { id = "FBWYNLJ-VALD4DG-KTPZ4HG-AUCST5P-NOPJETV-F4I4WLM-MGRMXRS-XAHGLA2"; };
+        "MacBook Pro"     = { id = "2NM42ZS-OPYWZNW-VA42QTK-2RKIB5Y-3QNBNPT-KNH6LJL-L7Q2BPI-PK6YKAZ"; };
       };
       folders = {
         "Downloads" = {
           id = "drfnf-ayqbj";
           path = "/home/kreid/Downloads";
-          devices = [ "MacBook Air" "MacBook Pro" ];
+          devices = [ "Windows desktop" "MacBook Air" "MacBook Pro" ];
           versioning = {
             type = "trashcan";
             params.cleanoutDays = "30";
