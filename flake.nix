74e625683927a6a79b3037067ed9b9cb6514c5a5
Kyle Reid <kyle.reid@gmail.com>
2026-02-20 04:53:12 -0500

    feat: add nix fmt & develop support

--- flake.nix
+++ flake.nix
@@ -6,11 +6,43 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.homeserver = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./hosts/homeserver
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
       ];
+
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          f pkgs
+        );
+
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-tree);
+
+      devShells = forAllSystems (pkgs: {
+
+        default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            nixfmt-tree
+            nixf-diagnose
+          ];
+        };
+      });
+
+      nixosConfigurations.homeserver = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/homeserver
+        ];
+      };
     };
-  };
 }
